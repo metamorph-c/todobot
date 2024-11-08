3e6b60d9678b28f75f4cdd2640c623ce77504a10
Metamorphius <koblabcorp@gmail.com>
2024-11-09 00:42:11 +0300

    load database configuration data

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,28 @@ import (
 )
 
 const (
-	envLoadError = "Warning: .env file not found, loading from system environment variables"
-	envReadError = "failed to load environment variables from .env file: %v"
+	envLoadError    = "Warning: .env file not found, loading from system environment variables"
+	envReadError    = "failed to load environment variables from .env file: %v"
+	configReadError = "Warning: db config not found: %v"
 )
 
 type Config struct {
 	// Telegram API token for authenticating requests
 	Token string `env:"TELEGRAM_APITOKEN" env-required:"true"`
+
+	// Path to the database configuration file
+	DbPath string `env:"DB_PATH" env-required:"true"`
+
+	Database `yaml:"database"`
+}
+
+type Database struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DbName   string `yaml:"db_name"`
+	Dialect  string `yaml:"dialect"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -23,11 +38,15 @@ func LoadConfig() (*Config, error) {
 		log.Println(envLoadError)
 	}
 
-	var cnf Config
+	var cfg Config
 
-	if err := cleanenv.ReadEnv(&cnf); err != nil {
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		return nil, fmt.Errorf(envReadError, err)
 	}
 
-	return &cnf, nil
+	if err := cleanenv.ReadConfig(cfg.DbPath, &cfg); err != nil {
+		log.Printf(configReadError, err)
+	}
+
+	return &cfg, nil
 }
