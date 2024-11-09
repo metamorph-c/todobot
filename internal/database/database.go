6e6d40b5432d687d6583fddf50a0b03db681b190
Metamorphius <koblabcorp@gmail.com>
2024-11-09 14:03:53 +0300

    add automatic database creation and migration, refactor databse setup

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,11 +8,20 @@ import (
 )
 
 const (
+	// Dialects
 	mysqlDialect = "mysql"
+
+	// Errors
+	failedToCreateDb       = "failed to create database: %v"
+	UsupportedDialectError = "unsupported database dialect: %s"
+
+	// Queries
+	createDataBaseIfNotExist = "CREATE DATABASE IF NOT EXISTS %s"
 )
 
 type Database interface {
 	Connect() (*gorm.DB, error)
+	AutoMigrate(*gorm.DB, ...interface{}) error
 }
 
 func NewDatabase(cfg *config.Database) (Database, error) {
@@ -23,9 +32,9 @@ func NewDatabase(cfg *config.Database) (Database, error) {
 			Port:     cfg.Port,
 			User:     cfg.User,
 			Password: cfg.Password,
-			DbName:   cfg.DbName,
+			Name:     cfg.DbName,
 		}, nil
 	default:
-		return nil, fmt.Errorf("unsupported database dialect: %s", cfg.Dialect)
+		return nil, fmt.Errorf(UsupportedDialectError, cfg.Dialect)
 	}
 }
--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -8,7 +8,11 @@ import (
 )
 
 const (
-	mysqlDNS = "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	mysqlDsn       = "%s:%s@tcp(%s:%s)/"
+	mysqlDsnWithDb = "%s%s?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// Errors
+	connectError = "failed to connect to MySQL server at %s:%s - %v"
 )
 
 type MySQLDatabase struct {
@@ -16,10 +20,25 @@ type MySQLDatabase struct {
 	Port     string
 	User     string
 	Password string
-	DbName   string
+	Name     string
 }
 
-func (db *MySQLDatabase) Connect() (*gorm.DB, error) {
-	dsn := fmt.Sprintf(mysqlDNS, db.User, db.Password, db.Host, db.Port, db.DbName)
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func (mysqlDB *MySQLDatabase) Connect() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(mysqlDsn, mysqlDB.User, mysqlDB.Password, mysqlDB.Host, mysqlDB.Port)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf(connectError, mysqlDB.Host, mysqlDB.Port, err)
+	}
+
+	sql := fmt.Sprintf(createDataBaseIfNotExist, mysqlDB.Name)
+	if err := db.Exec(sql).Error; err != nil {
+		return nil, fmt.Errorf(failedToCreateDb, err)
+	}
+
+	dsnWithDb := fmt.Sprintf(mysqlDsnWithDb, dsn, mysqlDB)
+	return gorm.Open(mysql.Open(dsnWithDb), &gorm.Config{})
+}
+
+func (mysqlDB *MySQLDatabase) AutoMigrate(db *gorm.DB, models ...interface{}) error {
+	return db.AutoMigrate(models)
 }
